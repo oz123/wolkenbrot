14da61a8525e4cfbc6eb8ef28c4630e01f57f418
Oz N Tiram <oz.tiram@gmail.com>
2021-07-31 23:29:18 +0200

    WIP: continue work on openstack builder

--- wolkenbrot/os.py
+++ wolkenbrot/os.py
@@ -1,6 +1,10 @@
-from datetime import date
-from getpass import getuser
+import time
+from datetime import date, datetime
 from .util import timeout, printr, printg, printy, random_name, SSHClient
+
+from paramiko.ssh_exception import (NoValidConnectionsError,
+                                    AuthenticationException)
+import paramiko
 
 
 class OpenStackBuilder:
@@ -11,12 +15,13 @@ class OpenStackBuilder:
         self.desc = config_params["description"]
         self.region = config_params["region"]
         self.instance_type = config_params["instance_type"]
-        self.ami = config_params["base_image"][self.region]
+        self.image = config_params["base_image"]
         self.config = config_params
         self.tags = config_params.get("tags")
         self.key = None
         self.instance = None
         self.ssh_client = None
+        self.sec_group_id = None
 
     def __enter__(self):
         """
@@ -66,14 +71,58 @@ class OpenStackBuilder:
 
     @timeout(600, "launch instance timed out!")
     def launch(self):
-        pass
+        machine = self.client.create_server(
+            'wolkenbrot-image-creator-{}'.format(datetime.now().strftime("%Y-%m-%d_%H:%M")),  # noqa
+            flavor=self.config['flavor'],
+            network=self.config['networks'],
+            security_groups=self.sec_group_id,
+            image=self.image.id,
+            key_name=self.key.id,
+            userdata='manage_etc_hosts: true'
+        )
+        self.instance = machine
 
     def wait_for_status(self, status):
-        pass
+        """
+        Wait of OS Instance to reach a certain status
 
+        BULDING
+        RUNNING
+        DELELTING
+        """
+
+        while self.instance.status != status:
+            print(
+                "Instance: %s is in in %s state, sleeping for 5 more seconds",
+            self.instance.name, self.instance.status)
+            self.instance = self.client.get_server(self.instance.id)
+            time.sleep(5)
+        
     @timeout(600, "waiting for SSH timesout!")
     def wait_for_ssh(self):
-        pass
+        if self.instance.public_v4:
+            ip = self.instance.public_v4
+        else:
+            ip = self.instance['addresses'][self.config['networks'][0]][0]['addr']
+
+        print(f"Connecting to {ip} using key {self.key.name}")
+
+        for i in range(0, 15):
+            try:
+                # TODO: fix hard coded user here
+                client = SSHClient(ip, 22, 'ubuntu', None,
+                                   self.key.private_key, None)
+                return client
+            except paramiko.ssh_exception.PasswordRequiredException as excep:
+                raise excep
+            except (NoValidConnectionsError, TimeoutError,
+                    AuthenticationException) as e:
+                print(f'Connection failed, it is likely that server is not '
+                      f'ready yet. Wait 4 seconds and retry. {e}')
+                time.sleep(4)
+
+        raise ValueError('Could not connect to the machine via SSH.')
+
 
     @timeout(1200, "Copying files took too long ...")
     def copy_files(self):
