940bdda4cf9ef4ee3b229c9feaccde00b7dfc543
Oz N Tiram <oz.tiram@gmail.com>
2021-09-28 22:59:04 +0200

    AWS: fix tiny bugs introduced by refactoring

--- wolkenbrot/aws.py
+++ wolkenbrot/aws.py
@@ -133,13 +133,12 @@ class AWSBuilder(Builder):
         printy("The instance is now running ...")
         # The instance is running, but we give it 60 more seconds for running
         # SSHD
-        ip_addr = self.instance.public_ip_address,
+        ip_addr = self.instance.public_ip_address
         print(f"Connecting to {ip_addr} using key {self.key.name}")
-
         for i in range(0, 15):
             try:
                 self.ssh_client = SSHClient(ip_addr, 22, self.config["user"], None,
-                                   self.key.private_key, None)
+                                   self.key.key_material, None)
             except paramiko.ssh_exception.PasswordRequiredException as excep:
                 raise excep
             except (NoValidConnectionsError, TimeoutError,
