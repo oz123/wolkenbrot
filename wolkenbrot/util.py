80d698083a74cabba0b5c6f88bd0a8caa165de06
Oz N Tiram <oz.tiram@gmail.com>
2021-10-16 15:42:31 +0200

    Add license header to all files

--- wolkenbrot/util.py
+++ wolkenbrot/util.py
@@ -1,11 +1,18 @@
-# ===========================================================================
-# This file is part of Wolkenbrot.
+# This file is part of wolkenbrot.
+
+# Copyright (c) 2017 - 2021 Oz Tiram <oz.tiram@gmail.com>
 #
-# Wolkenbrot is free software; you can redistribute it and/or modify
-# it under the terms of the MIT license.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 # ============================================================================
-# Copyright (C) 2017-2019 Oz Nahum Tiram <oz.tiram@gmail.com>
-# ============================================================================
+
 import functools
 import os
 import random
